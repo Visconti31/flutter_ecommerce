4c0aaf13f485e36eedfd9f14229b49d403ef2b18
Visconti31 <visconti.joao@gmail.com>
2024-12-15 12:11:36 +0000

    Create OnBoardingNextButton

---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/utils/constants/colors.dart';
+import 'package:flutter_ecommerce/utils/constants/sizes.dart';
+import 'package:flutter_ecommerce/utils/device/device_utility.dart';
+import 'package:flutter_ecommerce/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDevicesUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            backgroundColor: dark ? TColors.light : TColors.black,
+            iconColor: dark ? TColors.black : TColors.light),
+        child: const Icon(Iconsax.arrow_right_3),
+      ),
+    );
+  }
+}
