f60fdb4aeb8acc70acece64c2bb3d329147de3c2
Visconti31 <visconti.joao@gmail.com>
2024-12-15 16:42:28 +0000

    Refactoring LoginScreen and some adjustment

--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -21,7 +21,7 @@ class OnboardingController extends GetxController {
   // Update current index and jump to the next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      Get.to(LoginScreen());
+      Get.to(() => const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
@@ -32,6 +32,6 @@ class OnboardingController extends GetxController {
   void skipPage() {
     // currentPageIndex.value = 2;
     // pageController.jumpToPage(2);
-    Get.to(LoginScreen());
+    Get.to(() => const LoginScreen());
   }
 }
