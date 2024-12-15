784c525b55a6fcd7a77f36a52618268d9d05e826
Visconti31 <visconti.joao@gmail.com>
2024-12-15 16:26:02 +0000

    Finishing design LoginScreen before refactoring

--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/common/styles/spacing_styles.dart';
+import 'package:flutter_ecommerce/utils/constants/colors.dart';
 import 'package:flutter_ecommerce/utils/constants/image_strings.dart';
 import 'package:flutter_ecommerce/utils/constants/sizes.dart';
 import 'package:flutter_ecommerce/utils/constants/text_strings.dart';
 import 'package:flutter_ecommerce/utils/helpers/helper_functions.dart';
+import 'package:get/utils.dart';
 import 'package:iconsax/iconsax.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -46,66 +48,138 @@ class LoginScreen extends StatelessWidget {
 
               // Form
               Form(
-                child: Column(
-                  children: [
-                    // Email
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Iconsax.direct_right),
-                        labelText: TTexts.email,
-                      ),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-                    // Password
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Iconsax.password_check),
-                        labelText: TTexts.password,
-                        suffixIcon: Icon(Iconsax.eye_slash),
-                      ),
-                    ),
-
-                    const SizedBox(height: TSizes.spaceBtwInputFields / 2),
-
-                    // Remember me and Forget Password
-                    Row(
-                      children: [
-                        // Remember me
-                        Row(
-                          children: [
-                            Checkbox(value: true, onChanged: (value) {}),
-                            const Text(TTexts.rememberMe),
-                          ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: TSizes.spaceBtwSections),
+                  child: Column(
+                    children: [
+                      // Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.direct_right),
+                          labelText: TTexts.email,
                         ),
-
-                        // Forget Password
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text(TTexts.forgetPassword))
-                      ],
-                    ),
-
-                    const SizedBox(height: TSizes.spaceBtwSections),
-
-                    // SignIn Button
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text(TTexts.signIn),
                       ),
-                    ),
-
-                    // Create Account Button
-                    SizedBox(
-                      width: double.infinity,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: const Text(TTexts.createAccount),
+                      const SizedBox(height: TSizes.spaceBtwInputFields),
+                      // Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.password_check),
+                          labelText: TTexts.password,
+                          suffixIcon: Icon(Iconsax.eye_slash),
+                        ),
                       ),
-                    )
-                  ],
+
+                      const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+                      // Remember me and Forget Password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Remember me
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (value) {}),
+                              const Text(TTexts.rememberMe),
+                            ],
+                          ),
+
+                          // Forget Password
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(TTexts.forgetPassword))
+                        ],
+                      ),
+
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      // SignIn Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(TTexts.signIn),
+                        ),
+                      ),
+
+                      const SizedBox(height: TSizes.spaceBtwItems),
+
+                      // Create Account Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: const Text(TTexts.createAccount),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+              ),
+
+              // Divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Divider(
+                      color: dark ? TColors.darkGrey : TColors.grey,
+                      thickness: 0.5,
+                      indent: 60,
+                      endIndent: 5,
+                    ),
+                  ),
+                  Text(
+                    TTexts.orSignInWith.capitalize!,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Flexible(
+                    child: Divider(
+                      color: dark ? TColors.darkGrey : TColors.grey,
+                      thickness: 0.5,
+                      indent: 5,
+                      endIndent: 60,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Footer
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: TColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.google),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: TColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.facebook),
+                      ),
+                    ),
+                  ),
+                ],
               )
             ],
           ),
